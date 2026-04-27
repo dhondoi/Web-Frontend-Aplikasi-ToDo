dbc93b4de16b02347dc0be0f6ca611d38109c0ad
Doni Firmansyah <donifirmansyah33@gmail.com>
2026-04-27 18:57:22 +0700

    Generate List Elemen ToDo

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,14 @@
         });
 
         document.addEventListener(RENDER_EVENT, function () {
-            console.log(todos);
+            // console.log(todos);
+            const uncompletedTODOList = document.getElementById('todos');
+            uncompletedTODOList.innerHTML = '';
+
+            for (const todoItem of todos) {
+                const todoElement = makeTodo(todoItem);
+                uncompletedTODOList.append(todoElement);
+            }
         });
 
         function addTodo() {
@@ -39,6 +46,26 @@
                 timestamp,
                 isCompleted
             }
+        }
+
+        //creating element for item list
+        function makeTodo(todoObject) {
+            const textTitle = document.createElement('h2');
+            textTitle.innerText = todoObject.task;
+
+            const textTimestamp = document.createElement('p');
+            textTimestamp.innerText = todoObject.timestamp;
+
+            const textContainer = document.createElement('div');
+            textContainer.classList.add('inner');
+            textContainer.append(textTitle, textTimestamp);
+
+            const container = document.createElement('div');
+            container.classList.add('item', 'shadow');
+            container.append(textContainer);
+            container.setAttribute('id', `todo-${todoObject.id}`);
+
+            return container;
         }
     });
 })();
