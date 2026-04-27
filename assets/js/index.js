765f8a43b06f250e863b39b2bf3dbb7971684736
Doni Firmansyah <donifirmansyah33@gmail.com>
2026-04-27 17:46:52 +0700

    Add ToDo Business Logic
    creating function add todo

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,5 +7,19 @@
 
             addTodo();
         });
+
+        function addTodo() {
+            // getting data inside form
+            const textTodo = document.getElementById('title').value;
+            const timestamp = document.getElementById('date').value;
+
+            // creating and inserting object
+            const generatedID = generateId();
+            const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+            todos.push(todoObject);
+
+            // create new event
+            document.dispatchEvent(new Event(RENDER_EVENT));
+        }
     });
 })();
